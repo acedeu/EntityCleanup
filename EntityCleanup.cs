45b71feb4574eaa7678c83eb72a2d370d4253218
aced <104261012+acedeu@users.noreply.github.com>
2024-05-04 02:15:53 +0100

    Add files via upload

---
+++ EntityCleanup.cs
@@ -0,0 +1,36 @@
+﻿using Oxide.Core;
+using Oxide.Core.Plugins;
+
+namespace Oxide.Plugins
+{
+    [Info("EntityCleanup", "acedrust", "1.0.6")]
+    class EntityCleanup : RustPlugin
+    {
+        private Timer cleanupTimer;
+
+        private void OnServerInitialized()
+        {
+            cleanupTimer = timer.Repeat(60f, 0, () =>  // TIMER LOGIC 60 SECONDS = EVERY CLEANUP = 45 SECONDS ALERT BEFORE HAND
+            {
+                BroadcastCleanupWarning();
+                timer.Once(45f, ClearDroppedItems);
+            });
+        }
+
+        private void ClearDroppedItems()
+        {
+            rust.RunServerCommand("global.cleardroppeditems");
+            Puts("Dropped items cleared.");
+        }
+
+        private void BroadcastCleanupWarning()
+        {
+            PrintToChat("Entity cleanup will occur in 45 seconds.");
+        }
+
+        private void Unload()
+        {
+            cleanupTimer?.Destroy();
+        }
+    }
+}
